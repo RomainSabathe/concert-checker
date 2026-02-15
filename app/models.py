d00159dcf9f71891284435625eb3b8046d4408a4
Romain Sabathe <RSabathe@gmail.com>
2026-02-15 11:43:43 +0100

    Using Mapped instead of Columns (sqlalchemy 2)

--- app/models.py
+++ app/models.py
@@ -1,30 +1,32 @@
-from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship
+from datetime import datetime
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database import Base
 
 
 class Artist(Base):
     __tablename__ = "artists"
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
-    concerts = relationship("Concert", back_populates="artist")
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column()
+    concerts: Mapped[list["Concert"]] = relationship(back_populates="artist")
 
 
 class Venue(Base):
     __tablename__ = "venues"
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
-    city = Column(String, nullable=False)
-    country = Column(String, nullable=False)
-    country_code = Column(String, nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column()
+    city: Mapped[str] = mapped_column()
+    country: Mapped[str | None] = mapped_column()
+    country_code: Mapped[str | None] = mapped_column()
 
 
 class Concert(Base):
     __tablename__ = "concerts"
-    id = Column(Integer, primary_key=True)
-    date = Column(DateTime, nullable=False)
-    artist_id = Column(Integer, ForeignKey("artists.id"), nullable=False)
-    venue_id = Column(Integer, ForeignKey("venues.id"), nullable=True)
-    artist = relationship("Artist", back_populates="concerts")
-    venue = relationship("Venue")
+    id: Mapped[int] = mapped_column(primary_key=True)
+    date: Mapped[datetime] = mapped_column()
+    artist_id: Mapped[int] = mapped_column(ForeignKey("artists.id"))
+    venue_id: Mapped[int | None] = mapped_column(ForeignKey("venues.id"))
+    artist: Mapped["Artist"] = relationship(back_populates="concerts")
+    venue: Mapped["Venue | None"] = relationship()
